f9713d9337c594cced6bc16696ac8eafaa9b329b
Thimblee <65766417+Thimblee@users.noreply.github.com>
2022-10-15 17:07:36 +0900

    add: scraping_distance

--- Disney/urls.py
+++ Disney/urls.py
@@ -5,4 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('scrape', views.scrape, name='scrape'),
+    path('bs', views.bs, name='bs'),
+    path('fetch_attractions', views.fetch_attractions, name='fetch_attractions'),
+    path('scrape_attractions_urls', views.scrape_attractions_urls, name='scrape_attractions_urls'),
+    path('scrape_attractions_names', views.scrape_attractions_names, name='scrape_attractions_names'),
+    path('test', views.test, name='test'),
+    path('source_attractions', views.source_attractions, name='source_attractionssource_attractions'),
+    path('scrape_attractions_meter_and_to', views.scrape_attractions_meter_and_to, name='scrape_attractions_meter_and_to'),
 ]
