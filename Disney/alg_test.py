b2a0f47e8ba33796123f1f0d889e37fa04702b19
Thimblee <65766417+Thimblee@users.noreply.github.com>
2022-11-25 15:12:06 +0900

    update: alg_wada.py; Algorithm termination condition changed from iteration to time

--- Disney/alg_test.py
+++ Disney/alg_test.py
@@ -6,14 +6,14 @@ import numpy as np
 
 if __name__ == '__main__':
     # np.random.seed(28)
-    # ipso = alg_wada.IPSO(16, 0.2, 0.3, i=1000, display_flg=False)
+    # ipso = alg_wada.IPSO(16, 0.2, 0.3, display_flg=True)
     # route, time = alg_util.wrapper_alg(ipso, './Disney/attractions_distances_2.csv', './wait_time_data_20221105.csv', True)
     # print('route:', route)
     # print('time:', time)
 
     # np.random.seed(28)
-    # ga = alg_wada.GA(256, 32, 1.0, 1.0, i=1000)
-    # route, time = alg_util.wrapper_alg(ipso, './Disney/attractions_distances_2.csv', './wait_time_data_20221105.csv', True)
+    # ga = alg_wada.GA(32, 4, 1.0, 1.0)
+    # route, time = alg_util.wrapper_alg(ga, './Disney/attractions_distances_2.csv', './wait_time_data_20221105.csv', True)
     # print('route:', route)
     # print('time:', time)
 
@@ -22,9 +22,9 @@ if __name__ == '__main__':
     # print('route:', route)
     # print('time:', time)
 
-    for i in range(10):
-        np.random.seed(i)
-        ipso = alg_wada.IPSO(16, 0.2, 0.3, i=1000, display_flg=False)
-        route, time = alg_util.wrapper_alg(ipso, './Disney/attractions_distances_2.csv', './wait_time_data_20221105.csv', True)
-        print('route:', route)
-        print('time:', time)
+    # for i in range(10):
+    #     np.random.seed(i)
+    #     ipso = alg_wada.IPSO(16, 0.2, 0.3, i=1000, display_flg=False)
+    #     route, time = alg_util.wrapper_alg(ipso, './Disney/attractions_distances_2.csv', './wait_time_data_20221105.csv', True)
+    #     print('route:', route)
+    #     print('time:', time)
